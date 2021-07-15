4aa2831e7a3ddabbeefe0f121b23d1a4335e1631
German Dobler <german.crack@hotmail.com>
2021-07-15 19:39:16 -0300

    agregando cosas

---
+++ js/menu.js
@@ -0,0 +1,5 @@
+let BtnMenu = document.getElementById('btn-menu');
+let mainNav = document.getElementById('main-nav');
+mainNav.addEventListener('click', function(){
+    menu.classList.toggle('mostrar');
+    });
