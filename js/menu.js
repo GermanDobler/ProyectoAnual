51b464ec98300da8c8f1c04723bf7146e3ff118d
German Dobler <german.crack@hotmail.com>
2021-08-14 21:26:56 -0300

    agregando menu

--- js/menu.js
+++
@@ -1,5 +0,0 @@
-let BtnMenu = document.getElementById('btn-menu');
-let mainNav = document.getElementById('main-nav');
-mainNav.addEventListener('click', function(){
-    menu.classList.toggle('mostrar');
-    });
